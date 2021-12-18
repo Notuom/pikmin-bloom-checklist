4b8c44b7173a514892ffdbece32c24214769baf5
Notuom <thibault.karl@gmail.com>
2021-12-17 17:25:56 -0800

    chore: deploy to gh-pages

---
+++ _next/static/FQ7NyjjJZHYGelMcs-53j/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/419c1070d573f289.css","static/chunks/pages/index-aa47ee2f8ad437ab.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
