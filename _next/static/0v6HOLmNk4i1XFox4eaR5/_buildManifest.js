82e49c672d8265177a78ebdfc33c328ab16157f4
Notuom <thibault.karl@gmail.com>
2023-02-15 03:48:31 +0000

    Deploying to gh-pages from @ Notuom/pikmin-bloom-checklist@1f0daf9b4e8109e8c691a16f1f65b405e57ec6c7 🚀

---
+++ _next/static/0v6HOLmNk4i1XFox4eaR5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/869-805f508ab622e5fa.js","static/css/4447ea716019acb3.css","static/chunks/pages/index-01f2eb3acd2375a5.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/JQMo7ZunQdsEJ1dlVaaiB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/869-805f508ab622e5fa.js","static/css/4447ea716019acb3.css","static/chunks/pages/index-4eec1a75c078c97d.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
