d60269f6b78853f87d78bf80f8fe6750fd046ae4
Notuom <thibault.karl@gmail.com>
2023-08-30 03:37:32 +0000

    Deploying to gh-pages from @ Notuom/pikmin-bloom-checklist@b43bd84f7432160f5b38f3f29c724d26cea03407 🚀

---
+++ _next/static/FsuRLfK0-WE_3pFHtrFlR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/869-805f508ab622e5fa.js","static/css/23466a4abaefcc31.css","static/chunks/pages/index-e388d48d62c28419.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
